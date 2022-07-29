27b18dbac83c1251e075197910299bf5bfde25c3
TemporaAndromeda <ddeyphysics2010@gmail.com>
2022-07-29 23:41:43 +0530

    app name change

--- vcommunity/settings.py
+++ vcommunity/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'matterroom.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'matter-room.herokuapp.com']
 
 
 # Application definition
